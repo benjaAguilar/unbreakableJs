ec5fed99761b2d6a69e7f53fa9e6ecbb99ed742d
Benja Aguilar <benjaaguilar02@gmail.com>
2026-02-04 12:36:17 -0300

    feat: add linkedList Class
    this class is capable of keepTrack of his value and adjacent nodes.
    with this we can chekck previous and next steps keeping track of the
    place that we are at.
    
    add tail() function
    this function is capable to go to the tail of the linked list and return
    it

---
+++ src/linkedList.ts
@@ -0,0 +1,38 @@
+type Val = {
+  type: string;
+  code: string;
+};
+
+export class LinkedList {
+  val: Val;
+  next: LinkedList | null;
+  prev: LinkedList | null;
+
+  constructor(val: Val, next?: LinkedList | null, prev?: LinkedList | null) {
+    this.val = val;
+    this.next = next instanceof LinkedList ? next : null;
+    this.prev = prev instanceof LinkedList ? prev : null;
+  }
+
+  getArrayOfSteps(): Val[] {
+    const arr = [];
+    let current: LinkedList | null = this;
+
+    while (current !== null) {
+      arr.push(current.val);
+      current = current.next;
+    }
+
+    return arr;
+  }
+
+  tail(): LinkedList {
+    let current: LinkedList = this;
+
+    while (current.next !== null) {
+      current = current.next;
+    }
+
+    return current;
+  }
+}
